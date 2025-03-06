d066befb55155e0f0dc4a38e2123baa27ee06bee
berluk12@gmail.com <berluk12@gmail.com>
2025-03-06 14:00:23 +0100

    New docker scripts

--- windows-build-script.bat
+++ windows-build-script.bat
@@ -3,6 +3,8 @@ setlocal
 
 set "initial_dir=%CD%"
 
+git pull
+
 set "folders=bervan-utils common-vaadin file-storage-app pocket-app spreadsheet-app project-mgmt-app canvas-app streaming-platform-app interview-app english-text-stats-app learning-language-app shopping-stats-server-app my-tools-vaadin-app"
 
 for %%f in (%folders%) do (
