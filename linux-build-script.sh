6934361dc5cd03f9f4cbcdf28c3eb58159af53a7
berluk12@gmail.com <berluk12@gmail.com>
2025-05-22 14:55:42 +0200

    New server onboarding

---
+++ linux-build-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+initial_dir=$(pwd)
+
+git pull
+
+folders="bervan-utils common-vaadin file-storage-app pocket-app spreadsheet-app project-mgmt-app canvas-app streaming-platform-app interview-app english-text-stats-app learning-language-app shopping-stats-server-app my-tools-vaadin-app"
+
+for folder in $folders; do
+  if [ -d "$folder" ]; then
+    cd "$folder"
+    echo "Processing folder: $folder"
+    git stash
+    git pull
+    if [ "$folder" == "bervan-utils" ]; then
+      docker build -t bervan-utils .
+    fi
+    cd ..
+  else
+    echo "Folder not found: $folder"
+  fi
+done
+
+cd "$initial_dir"
+docker-compose --env-file .env_my_tools up --build -d
